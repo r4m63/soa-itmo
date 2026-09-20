7ab3814bddcb5300329833fb88e7fe3b38b4c793
ramil <rm.tj.777@gmail.com>
2026-09-20 12:16:29 +0300

    init

---
+++ src/test/java/com/itmo/soa/SoaItmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.soa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaItmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
